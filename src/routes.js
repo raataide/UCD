b4d53f5345167be72fc1abbf35fbf147d61de54c
Rodrigo Ataíde <rodrigo.a.ataide@accenture.com>
2020-06-01 23:01:32 -0300

    Home screen

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// import Login from "./pages/login/";
+// import Exames from "./pages/exames/";
+import Home from "./pages/home/";
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Home" component={Home} />
+        {/* <AppStack.Screen name="Login" component={Login} /> */}
+        {/* <AppStack.Screen name="Exames" component={Exames} /> */}
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
